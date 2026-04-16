66a781bd642c8a8bdb835cc0003ba6440b48be17
sara charafi <elkbircharafi8@gmail.com>
2026-04-16 12:22:14 +0100

    modifier sur table student

--- script.sql
+++ script.sql
@@ -1,41 +1,63 @@
-CREATE DATABASE edusync;
-USE edusync;
-CREATE TABLE roles (
+ CREATE DATABASE edusync;
+ use edusync;
+ CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    label VARCHAR(50) NOT NULL
+    label VARCHAR (50) NOT NULL
 );
+
+INSERT INTO roles (label)
+VALUES('Admin'),('Prof'),('Student');
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(100),
-    lastname VARCHAR(100),
-    email VARCHAR(150) UNIQUE,
-    password VARCHAR(255),
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    email VARCHAR (100) NOT NULL UNIQUE,
+    pasword VARCHAR(255) NOT NULL,
     role_id INT,
-    
     FOREIGN KEY (role_id) REFERENCES roles(id)
-    ON DELETE RESTRICT
 );
+
+INSERT INTO users (firstname, lastname ,email ,pasword ,role_id)
+VALUES
+('Khaoula' , 'Admin' ,'khawla1@gmail' ,'123456' ,1),
+('hamza' , 'Prof' ,'hamza2@gmail' ,'456789' ,2),
+('bilal' , 'Prof' ,'bilal3@gmail' ,'135790' ,3),
+('sara' , 'Student' ,'sara1@gmail' ,'325498' ,1);
+
+
 CREATE TABLE classes (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    classroom_number VARCHAR(50)
+    name VARCHAR (100) NOT NULL,
+    classroom_number VARCHAR(20)
 );
-CREATE TABLE students (
+
+
+INSERT INTO classes (name , classroom_number)
+VALUES 
+('Développeur web 2026' , 'a1'),
+('Développeur web 2025' , 'b2');
+
+CREATE TABLE courses (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (100) not null,
+    description TEXT,
+    total_hours INT,
+     user_id INT,
+     FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+INSERT INTO courses ( title ,description ,total_hours ,user_id)
+VALUES
+('html & css' , 'introduction au web' , 30 ,2),
+('javascript' , 'programmation web' , 40 ,2);
+
+CREATE table students (
+    id INT AUTO_INCREMENT PRIMARY key,
     dateOfBirth DATE,
     student_number VARCHAR(50) UNIQUE,
     user_id INT UNIQUE,
     class_id INT,
-
-    FOREIGN KEY (user_id) REFERENCES users(id)
-    ON DELETE CASCADE,
-
-
-
-
-
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (class_id) REFERENCES classes(id)
-
-    
-    ON DELETE SET NULL
 );
