743d988a06c953d920fb45a7039bc37c382bf1fc
sara charafi <elkbircharafi8@gmail.com>
2026-04-17 16:50:58 +0100

    insert values into users

--- seed.sql
+++ seed.sql
@@ -1,3 +1,10 @@
 INSERT INTO roles (label)
 VALUES('Admin'),('Prof'),('Student');
 
+INSERT INTO users (firstname, lastname, email, pasword, role_id)
+VALUES
+('amal', 'Prof', 'amal2@gmail.com', '456789', 2),
+('laila', 'Student', 'lailal3@gmail.com', '135790', 3),
+('sara', 'Admin', 'sara1@gmail.com', '325498', 1),
+('salah', 'prof', 'salah1@gmail.com', '325498', 2);
+
